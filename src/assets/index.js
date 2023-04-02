72c819ba88cecc8def786d31af1d002a90d04eb2
Muhammad Talha <talhahanfsd@gmail.com>
2023-04-03 01:15:57 +0500

    Change file type of assets

---
+++ src/assets/index.js
@@ -0,0 +1,50 @@
+import profileM from './images/profileM.png';
+import profile1 from './images/profile1.png';
+import profile2 from './images/profile2.png';
+import profile3 from './images/profile3.png';
+
+import add from './images/add.png';
+import arrow from './images/arrow.png';
+import arrowD from './images/arrowD.png';
+import arrowR from './images/arrowR.png';
+import back from './images/back.png';
+import foodG from './images/foodG.png';
+import foodP from './images/foodP.png';
+import graph from './images/graph.png';
+import hamburger from './images/hamburger.png';
+import healthG from './images/healthG.png';
+import healthP from './images/healthP.png';
+import kebab from './images/kebab.png';
+import send from './images/send.png';
+import taxiP from './images/taxi.png';
+import taxiG from './images/taxiG.png';
+import travelG from './images/travelG.png';
+import travelP from './images/travelP.png';
+import card from './images/card.png';
+
+let images = {
+  profileM,
+  profile1,
+  profile2,
+  profile3,
+  add,
+  arrow,
+  arrowD,
+  arrowR,
+  back,
+  foodG,
+  foodP,
+  graph,
+  hamburger,
+  healthG,
+  healthP,
+  kebab,
+  send,
+  taxiP,
+  taxiG,
+  travelG,
+  travelP,
+  card,
+};
+
+export default images;
